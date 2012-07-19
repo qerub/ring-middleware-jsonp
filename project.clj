c1937c915de8068365e858ec1f29ced3f8d35af5
Christoffer Sawicki <christoffer.sawicki@gmail.com>
2012-07-19 16:42:34 +0200

    Add :url to project definition

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject ring.middleware.jsonp "1.0.0-SNAPSHOT"
   :description "Ring middleware for JSONP"
+  :url "https://github.com/qerub/ring-middleware-jsonp"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [ring/ring-core "1.1.1"]])
