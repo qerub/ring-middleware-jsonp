924e488c1f734798bdfe01ff33f7fb34902b2e80
Christoffer Sawicki <christoffer.sawicki@gmail.com>
2012-07-20 19:20:55 +0200

    Mark version 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ring.middleware.jsonp "0.1.0"
+(defproject ring.middleware.jsonp "0.1.1-SNAPSHOT"
   :description "Ring middleware for JSONP"
   :url "https://github.com/qerub/ring-middleware-jsonp"
   :dependencies [[org.clojure/clojure "1.3.0"]
