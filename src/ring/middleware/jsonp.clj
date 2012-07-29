a256f7d3a011688acf995d06c888009e16ecb66d
Christoffer Sawicki <christoffer.sawicki@gmail.com>
2012-07-29 17:48:23 +0200

    Break out get-param and json-content-type?

--- src/ring/middleware/jsonp.clj
+++ src/ring/middleware/jsonp.clj
@@ -2,21 +2,25 @@
   (:use [ring.util.response :only (response content-type)]
         [clojure.test :only (deftest is)]))
 
-(defn- pad-json? [request response]
-  (and (or (get-in request [:params :callback])
-           (get-in request [:params "callback"]))
-       (re-matches #"application/json(;.*)?" (get-in response [:headers "Content-Type"] ""))))
+(defn- get-param [request param]
+  (or (get-in request [:params (keyword param)])
+      (get-in request [:params (name    param)])))
 
-(defn- add-padding-to-json [request response]
+(defn- json-content-type? [response]
+  (re-matches #"application/json(;.*)?" (get-in response [:headers "Content-Type"] "")))
+
+(defn- pad-json? [callback response]
+  (and callback (json-content-type? response)))
+
+(defn- add-padding-to-json [callback response]
   (-> response
       (content-type "application/javascript")
-      (update-in [:body] #(str (or (get-in request [:params :callback])
-                                   (get-in request [:params "callback"]))
-                               "(" % ");"))))
+      (update-in [:body] #(str callback "(" % ");"))))
 
 (defn wrap-json-with-padding [handler]
   (fn [request]
-    (let [response (handler request)]
-      (if (pad-json? request response)
-          (add-padding-to-json request response)
+    (let [callback (get-param request :callback)
+          response (handler request)]
+      (if (pad-json? callback response)
+          (add-padding-to-json callback response)
           response))))
