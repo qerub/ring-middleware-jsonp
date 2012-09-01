5c03da7c9c3b96f5752c93f45ab839a359e86a00
Christoffer Sawicki <christoffer.sawicki@gmail.com>
2012-09-01 21:27:20 +0200

    Bump ring-core version number

--- project.clj
+++ project.clj
@@ -2,4 +2,4 @@
   :description "Ring middleware for JSONP"
   :url "https://github.com/qerub/ring-middleware-jsonp"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [ring/ring-core "1.1.1"]])
+                 [ring/ring-core "1.1.3"]])
