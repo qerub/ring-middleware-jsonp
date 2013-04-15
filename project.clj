d97f03a09a7720b17758fb53f089a4975b4eebab
Christoffer Sawicki <christoffer.sawicki@gmail.com>
2013-04-15 10:51:59 +0200

    Add license to project definition

--- project.clj
+++ project.clj
@@ -1,5 +1,7 @@
 (defproject ring.middleware.jsonp "0.1.2-SNAPSHOT"
   :description "Ring middleware for JSONP"
   :url "https://github.com/qerub/ring-middleware-jsonp"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [ring/ring-core "1.1.3"]])
