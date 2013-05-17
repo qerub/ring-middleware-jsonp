83487082aa42ce7a623399cc5009f385f9106e5b
Christoffer Sawicki <christoffer.sawicki@gmail.com>
2013-05-17 12:35:47 +0200

    Add `re-matches?`

--- src/ring/middleware/jsonp.clj
+++ src/ring/middleware/jsonp.clj
@@ -13,8 +13,11 @@
   (or (get-in request [:params (keyword param)])
       (get-in request [:params (name    param)])))
 
+(defn- re-matches? [^java.util.regex.Pattern re s]
+  (.. re (matcher s) matches))
+
 (defn- json-content-type? [content-type]
-  (re-matches #"application/json(;.*)?" content-type))
+  (re-matches? #"application/json(;.*)?" content-type))
 
 (defn- pad-json? [callback response]
   (and callback (json-content-type? (get-in response [:headers "Content-Type"] ""))))
