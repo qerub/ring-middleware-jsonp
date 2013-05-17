91565ecb516a2c1514156f81fff5bc1c13332d47
Christoffer Sawicki <christoffer.sawicki@gmail.com>
2013-05-17 12:46:01 +0200

    Mark version 0.1.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ring.middleware.jsonp "0.1.2-SNAPSHOT"
+(defproject ring.middleware.jsonp "0.1.3"
   :description "Ring middleware for JSONP"
   :url "https://github.com/qerub/ring-middleware-jsonp"
   :license {:name "Eclipse Public License"
