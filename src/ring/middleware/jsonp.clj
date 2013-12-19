92dc30089d8f9599bbb436ecfa5678b88b6558cd
Markus Hjort <markus.hjort@reaktor.fi>
2013-12-19 18:26:28 +0200

    Use charset from original response also in padded response. If original response does not contain charset use system default.

--- src/ring/middleware/jsonp.clj
+++ src/ring/middleware/jsonp.clj
@@ -4,6 +4,7 @@
                     FileInputStream
                     InputStream
                     SequenceInputStream)
+           (java.nio.charset Charset)
            (java.util Enumeration
                       NoSuchElementException)
            (clojure.lang SeqEnumeration))
@@ -16,37 +17,48 @@
 (defn- re-matches? [^java.util.regex.Pattern re s]
   (.. re (matcher s) matches))
 
+(defn- get-charset [content-type]
+  (re-find #"(?<=charset=)[^;]*" content-type))
+
+(defn- get-charset-or-default [content-type]
+  (or (get-charset content-type)
+      (.name (Charset/defaultCharset))))
+
+(defn- get-content-type [response]
+  (get-in response [:headers "Content-Type"] ""))
+
 (defn- json-content-type? [content-type]
   (re-matches? #"application/(.*\+)?json(;.*)?" content-type))
 
 (defn- pad-json? [callback response]
-  (and callback (json-content-type? (get-in response [:headers "Content-Type"] ""))))
+  (and callback (json-content-type? (get-content-type response))))
 
-(defn- string->stream [^String s]
-  (ByteArrayInputStream. (.getBytes s)))
+(defn- string->stream [^String s ^String charset]
+  (ByteArrayInputStream. (.getBytes s charset)))
 
 (defn- concat-streams [xs]
   (->> xs seq SeqEnumeration. SequenceInputStream.))
 
-(defn- body->stream [body]
+(defn- body->stream [body charset]
   (cond (seq? body) (concat-streams
-                     (for [x body] (string->stream (str x))))
+                     (for [x body] (string->stream (str x) charset)))
         (instance? File body) (FileInputStream. body)
         (instance? InputStream body) body
-        (string? body) (string->stream body)
-        (nil? body) (string->stream "")
+        (string? body) (string->stream body charset)
+        (nil? body) (string->stream "" charset)
         :else (throw (Exception. (str "Don't know how to convert "
                                       (type body)
                                       " to an InputStream!")))))
 
 (defn- add-padding-to-json [callback response]
-  (-> response
-      (content-type "application/javascript")
-      (update-in [:body]
-                 #(concat-streams
-                   [(string->stream (str callback "("))
-                    (body->stream %)
-                    (string->stream ");")]))))
+  (let [charset (get-charset-or-default (get-content-type response))]
+    (-> response
+        (content-type (str "application/javascript; charset=" charset))
+        (update-in [:body]
+                   #(concat-streams
+                     [(string->stream (str callback "(") charset)
+                      (body->stream % charset)
+                      (string->stream ");" charset)])))))
 
 (defn wrap-json-with-padding [handler]
   (fn [request]
