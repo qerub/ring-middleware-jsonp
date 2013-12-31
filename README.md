4dc5aaa48e4c3aa39b2b6856673ae8c76c101eff
Christoffer Sawicki <christoffer.sawicki@gmail.com>
2013-12-31 01:01:03 +0100

    Mark version 0.1.4

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 Add the following to your `project.clj` `:dependencies`:
 
 ```clojure
-[ring.middleware.jsonp "0.1.3"]
+[ring.middleware.jsonp "0.1.4"]
 ```
 
 ## Usage
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ring.middleware.jsonp "0.1.4-SNAPSHOT"
+(defproject ring.middleware.jsonp "0.1.4"
   :description "Ring middleware for JSONP"
   :url "https://github.com/qerub/ring-middleware-jsonp"
   :license {:name "Eclipse Public License"
