9a1702fcfa552754afbbaed755b3f2a245ff48b5
Christoffer Sawicki <christoffer.sawicki@gmail.com>
2014-03-01 13:01:03 +0100

    Switch to a more exotic charset in test

--- test/ring/middleware/jsonp/test.clj
+++ test/ring/middleware/jsonp/test.clj
@@ -20,10 +20,10 @@
     (StringBufferInputStream. "{}")))
 
 (deftest test-json-with-padding-and-defined-charset
-  (let [handler  (constantly (content-type (response "{€}") "application/json; charset=utf-8"))
+  (let [handler  (constantly (content-type (response "{€}") "application/json; charset=iso-8859-15"))
         response ((wrap-json-with-padding handler) {:params {"callback" "f"}})]
-    (is (= (get-in response [:headers "Content-Type"]) "application/javascript; charset=utf-8"))
-    (is (= (slurp (:body response) :encoding "UTF-8") "f({€});"))))
+    (is (= (get-in response [:headers "Content-Type"]) "application/javascript; charset=iso-8859-15"))
+    (is (= (slurp (:body response) :encoding "ISO-8859-15") "f({€});"))))
 
 (deftest test-content-type-check
   (let [json-content-type? #'ring.middleware.jsonp/json-content-type?]
